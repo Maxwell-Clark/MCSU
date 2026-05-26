149c87f72fb56d53dd578dd08ba05d05deccdd79
Max Clark <maxwell6636@gmail.com>
2026-05-25 23:30:21 -0500

    remove footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
-import { Container, Group, ActionIcon, rem, Text } from '@mantine/core';
-import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
+import { Container, Text } from '@mantine/core';
 import classes from './Footer.module.css';
 
 export function Footer() {
@@ -7,17 +6,6 @@ export function Footer() {
     <div className={classes.footer}>
       <Container className={classes.inner}>
         <Text className={classes.brand}>Mindfulness Center of Southern Utah</Text>
-        <Group gap="md" className={classes.links} justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" variant="subtle" className={classes.socialIcon}>
-            <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" variant="subtle" className={classes.socialIcon}>
-            <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" variant="subtle" className={classes.socialIcon}>
-            <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-        </Group>
       </Container>
     </div>
   );
